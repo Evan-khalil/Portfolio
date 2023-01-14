9a6847b9f5ea3a3c3783b1c5bef5eafeee89212c
Evan Khalil <evankhalil1@hotmail.com>
2023-01-14 18:23:58 +0100

    newcommit

--- nav.js
+++ nav.js
@@ -4,14 +4,3 @@ $('a[href*=#]').click(function(event){
     }, 5000);
     event.preventDefault();
 });
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    $('body').css('display', 'flex');
-    $('body').css('flex-direction', 'column');
-    $('body').css('min-height', '1920px');
-    $('body').css('position', 'relative');
-    $('body').css('background', 'linear-gradient(45deg, hsla(216, 100%, 17%, 1) 0%, hsla(206, 80%, 29%, 1) 47%, hsla(206, 80%, 29%, 1) 83%)');
-    $('body').css('background', '-moz-linear-gradient(45deg, hsla(216, 100%, 17%, 1) 0%, hsla(206, 80%, 29%, 1) 47%, hsla(206, 80%, 29%, 1) 83%)');
-    $('body').css('background', '-webkit-linear-gradient(45deg, hsla(216, 100%, 17%, 1) 0%, hsla(206, 80%, 29%, 1) 47%, hsla(206, 80%, 29%, 1) 83%)');
-    $('body').css('background-attachment', 'fixed');
-
- }
